d34c1b8413cceba4677dcc66c78a0c8bab054708
Manuel <manuel.atenas.4@gmail.com>
2020-08-20 15:19:36 -0400

    se agrego mensaje error validacion de gastos

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,21 +1,52 @@
 import React, {useState} from 'react';
+import Error from './Error';
+
 
 
 
 function Formulario() {
     
-    
+    const [ nombreGasto, setNombreGasto ] = useState('');
+    const [cantidadGasto, setCantidadGasto] = useState(0);
+    const [error, setError] = useState(false);
+
+    //cuando el usuario agrega un gasto
+    const addGasto = (e) => {
+        e.preventDefault();
+
+        //validar
+        if(cantidadGasto < 1 || isNaN(cantidadGasto) || nombreGasto.trim() === '') {
+            setError(true);
+            return;
+        } 
+        setError(false);
+
+
+        //construir el gasto
+
+
+
+        //pasar el gasto al componente principal para listarlo
+
+
+
+        //resetear el form
+    }
+
     return (
         <form
-        
+            onSubmit={addGasto}
         >
             <h2>Agrega tus gastos</h2>
+            {error? <Error mensaje="Ingresaste mal los datos solicitados"></Error>   : null}
             <div className="campo">
                 <label htmlFor="">Nombre del gasto</label>
                 <input 
                     type="text"
                     className="u-full-width"
                     placeholder="Ej. transporte"
+                    value={nombreGasto}
+                    onChange={(e) => setNombreGasto(e.target.value)}
                 />
             </div>
             <div className="campo">
@@ -24,6 +55,8 @@ function Formulario() {
                     type="number"
                     className="u-full-width"
                     placeholder="Ej. 10.000"
+                    value={cantidadGasto}
+                    onChange={(e) => setCantidadGasto(parseInt(e.target.value,10))}
                 />
             </div>
             <input 
